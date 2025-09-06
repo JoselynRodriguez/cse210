80e704aa05014825ee5b9f444412e753513c98fd
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-06 09:41:22 -0500

    core requirements part 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+
+        int x = int.Parse(percentage);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulation you pass the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, You need to try again. You can do it!");
+        }
     }
 }
