e0248028ee663978507e1a2c677c54b7d5f39648
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-06 10:59:47 -0500

    core requirements part 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,27 +6,26 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
-        Console.Write("What is the magic number? ");
-        string magic = Console.ReadLine();
-        int magn = int.Parse(magic);
-
-        Console.Write("What is your guess?");
-        string guess = Console.ReadLine();
-        int number = int.Parse(guess);
-
-        if (number < magn)
+        int number = 18;
+        int guess = -1;
+        while (number != guess)
         {
-            Console.WriteLine("Higher");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guess it!");
+            }
         }
-        else if (number > magn)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guess it!");
-        }
-        
 
     }
 }
