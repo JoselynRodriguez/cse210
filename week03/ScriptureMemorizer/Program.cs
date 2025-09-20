d6ae31591806072e619808193edfe4ceac57cd69
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-20 13:17:58 -0500

    Creating the program bases

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string text = "Trust in the Lord with all thine heart; and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
+
+        Scripture scripture = new Scripture(reference, text);
+        
+
     }
+
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
+
+}
