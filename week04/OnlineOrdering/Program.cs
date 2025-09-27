3fbde5748a1b3f6a0892240a5459ade2edb83c07
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-27 15:09:43 -0500

    UPDATE

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+
+        Address address1 = new Address("123 Elm St", "Springfield", "IL", "USA");
+        Customer customer1 = new Customer("John Doe", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Notebook", 35678, 3.5, 4));
+        order1.AddProduct(new Product("Pen", 75780, 1.2, 10));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalPrice():0.00} ");
     }
 }
