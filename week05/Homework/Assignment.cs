a312a161425df0a295502219bf6bc85a898fa077
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-30 22:44:24 -0500

    creating assignment

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Dynamic;
+using Microsoft.VisualBasic;
+
+class Assignment
+{
+    private string _studentName;
+    private string _topicName;
+
+    public Assignment(string studentName, string topicName)
+    {
+        _studentName = studentName;
+        _topicName = topicName;
+    }
+    
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topicName;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topicName} ";
+    }
+}
